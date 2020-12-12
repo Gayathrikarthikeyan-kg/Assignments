c68c74614ae355a4528ca569006def06311c8258
Gayathri <Gayathri@LAPTOP-HABCFCPU>
2020-12-12 20:05:25 +0530

    Another simple change

--- Factorial.java
+++ Factorial.java
@@ -13,7 +13,7 @@ public class Factorial {
 	public static void main(String[] args) {
 
         int num = 5; 
-        System.out.println("Fact of "+ num + " is " + factorial(5)); 
+        System.out.println("Factorial of "+ num + " is " + factorial(5)); 
 
 	}
 
